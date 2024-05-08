3ef7f118e1d5140808a6ab3c94dd69359ed10418
jlucasoliveira <lucasosilva68@gmail.com>
2024-05-08 15:04:19 -0300

    feat(gender): add gender routes

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -4,6 +4,7 @@ import { App } from '@/components/Layout';
 
 const { BrandsRoutes } = lazyImport(() => import('@/features/brands'), 'BrandsRoutes');
 const { CategoriesRoutes } = lazyImport(() => import('@/features/categories'), 'CategoriesRoutes');
+const { GendersRoutes } = lazyImport(() => import('@/features/genders'), 'GendersRoutes');
 const { ProductRoutes } = lazyImport(() => import('@/features/products'), 'ProductRoutes');
 
 const privateRoutes: RouteObject[] = [
@@ -12,6 +13,7 @@ const privateRoutes: RouteObject[] = [
     element: <App />,
     children: [
       { path: 'brands/*', element: <BrandsRoutes /> },
+      { path: 'genders/*', element: <GendersRoutes /> },
       { path: 'categories/*', element: <CategoriesRoutes /> },
       { path: 'products/*', element: <ProductRoutes /> },
     ],
