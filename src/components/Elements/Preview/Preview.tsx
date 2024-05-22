26caf90afb53bd137e773e53ab7b8d3b487743b0
jlucasoliveira <lucasosilva68@gmail.com>
2024-05-22 09:20:43 -0300

    feat(Preview): preview only images

--- src/components/Elements/Preview/Preview.tsx
+++ src/components/Elements/Preview/Preview.tsx
@@ -1,31 +1,80 @@
 import {
+  ButtonProps,
+  IconButton,
+  Image,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Skeleton,
+  useBoolean,
+  useDisclosure,
 } from '@chakra-ui/react';
-import { PropsWithChildren } from 'react';
+import { Loading } from '..';
+import { ViewIcon } from '@chakra-ui/icons';
+import { AttachmentSigned } from '@/features/attachments/types';
+import { useEffect, useState } from 'react';
 
 type Props = {
   title: string;
-  isOpen: boolean;
-  onClose: () => void;
+  isLoading?: boolean;
+  file?: File;
+  attachment?: AttachmentSigned;
+  Button?: React.ElementType<ButtonProps>;
 };
 
-function Preview(props: PropsWithChildren<Props>) {
+function Preview(props: Props) {
+  const [base64, setBase64] = useState<string | undefined>();
+  const [imageLoaded, manager] = useBoolean();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    if (props.file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e?.target?.result) setBase64(e.target!.result as string);
+      };
+      reader.readAsDataURL(props.file);
+    }
+  }, [props.file]);
+
   return (
-    <Modal isCentered isOpen={props.isOpen} onClose={props.onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>{props.title}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody justifyContent="center" alignItems="center" display="flex">
-          {props.children}
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+    <>
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{props.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody justifyContent="center" alignItems="center" display="flex">
+            <Skeleton isLoaded={imageLoaded}>
+              <Image
+                fit="contain"
+                boxSize="400px"
+                src={base64 || props.attachment?.url}
+                alt={props.title}
+                onLoad={manager.on}
+              />
+            </Skeleton>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      {props.isLoading ? (
+        <Loading size="sm" />
+      ) : props.attachment?.url || props ? (
+        props.Button ? (
+          <props.Button onClick={onOpen} />
+        ) : (
+          <IconButton
+            aria-label="Ver arquivo"
+            variant="ghost"
+            icon={<ViewIcon />}
+            onClick={onOpen}
+          />
+        )
+      ) : null}
+    </>
   );
 }
 
