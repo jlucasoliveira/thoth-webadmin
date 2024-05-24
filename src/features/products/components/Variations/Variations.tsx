a888d650b07ac017ac432249f0615ded1bece69f
jlucasoliveira <lucasosilva68@gmail.com>
2024-05-24 12:03:21 -0300

    feat(products): add variation list

---
+++ src/features/products/components/Variations/Variations.tsx
@@ -0,0 +1,120 @@
+import { useCallback, useMemo } from 'react';
+import { ViewIcon } from '@chakra-ui/icons';
+import { ColumnDef } from '@tanstack/react-table';
+import { Control, useFieldArray } from 'react-hook-form';
+import { Button, Flex, IconButton, UseDisclosureReturn } from '@chakra-ui/react';
+import { useFilters } from '@/hooks/useFilters';
+import { Loading, Table } from '@/components/Elements';
+import { FieldsContainer } from '@/components/Form/FieldsContainer';
+import { useSendFileToBucket } from '@/features/attachments/api/sendFileToBucket';
+import { useDeleteAttachment } from '@/features/attachments/api/deleteAttachment';
+import { useVariationPartialUpdate } from '../../api/variations/partialUpdateVariation';
+import { useCreateVariation } from '../../api/variations/createVariation';
+import { useProductVariations } from '../../api/variations/getProductsVariation';
+import { ProductVariationModel } from '../../types';
+import { VARIATION_ID } from '../../utils/params';
+import { VariationModal, FormPayload } from './VariationModal';
+import { DeleteVariation } from '../Delete/DeleteVariation';
+import { performCreation } from './utils';
+
+type VariationsProps = Pick<UseDisclosureReturn, 'onOpen' | 'isOpen' | 'onClose'> & {
+  productId?: string;
+  isEdit?: boolean;
+  control: Control<any, any>;
+};
+
+function Variations({ productId, control, isEdit, isOpen, onOpen, onClose }: VariationsProps) {
+  const sendToBucket = useSendFileToBucket();
+  const createVariation = useCreateVariation();
+  const updateVariation = useVariationPartialUpdate();
+  const deleteAttachment = useDeleteAttachment();
+  const { query, addParam, getParam, removeParam } = useFilters({ context: 'variations' });
+  const { data, isFetching, isLoading } = useProductVariations({ productId, params: query });
+  const { remove, append } = useFieldArray({ control, name: 'variations' });
+  const variationId = useMemo(() => getParam(VARIATION_ID) ?? undefined, [getParam]);
+
+  const columns = useMemo<ColumnDef<ProductVariationModel>[]>(
+    () => [
+      { header: 'Nome', accessorKey: 'variation' },
+      { header: 'Código de referência', accessorKey: 'externalCode' },
+      { header: 'Valor', accessorKey: 'price' },
+      {
+        header: 'Ações',
+        accessorKey: 'id',
+        enableSorting: false,
+        cell: (info) => {
+          const id = info.getValue() as string;
+          return (
+            <Flex direction="row">
+              <IconButton
+                mr="2"
+                variant="ghost"
+                aria-label="Editor variação"
+                icon={<ViewIcon />}
+                size="xs"
+                onClick={() => {
+                  addParam(VARIATION_ID, id);
+                  onOpen();
+                }}
+              />
+              <DeleteVariation id={id} productId={productId} remove={remove} />
+            </Flex>
+          );
+        },
+      },
+    ],
+    [productId, remove, addParam, onOpen]
+  );
+
+  const handleOnClose = useCallback(() => {
+    removeParam(VARIATION_ID);
+    onClose();
+  }, [onClose, removeParam]);
+
+  async function onSubmit({ form, variation }: FormPayload) {
+    if (productId) {
+      performCreation(
+        createVariation.mutateAsync,
+        updateVariation.mutateAsync,
+        sendToBucket.mutateAsync,
+        deleteAttachment.mutateAsync,
+        productId,
+        { form, variation }
+      );
+    } else {
+      append(form);
+    }
+    handleOnClose();
+  }
+
+  if (isFetching && isLoading) return <Loading />;
+
+  return (
+    <>
+      <FieldsContainer title="Variações" templateColumn={1}>
+        <Button
+          isDisabled={productId ? !isEdit : false}
+          colorScheme="blue"
+          mb="5"
+          onClick={() => {
+            removeParam(VARIATION_ID);
+            onOpen();
+          }}
+        >
+          Adicionar Variação
+        </Button>
+        <VariationModal
+          key={variationId || 'add-variation'}
+          isEdit={isEdit}
+          isOpen={isOpen}
+          onClose={handleOnClose}
+          onSubmit={onSubmit}
+          productId={productId}
+        />
+      </FieldsContainer>
+      <Table columns={columns} data={data?.data ?? []} pages={data?.meta?.totalPages ?? 1} />
+    </>
+  );
+}
+
+export { Variations };
