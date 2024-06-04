987b3ed2827d6429ea6fb619d5a8b7fbc2daa72f
jlucasoliveira <lucasosilva68@gmail.com>
2024-06-04 11:26:03 -0300

    feat(stock): add product table

---
+++ src/features/stock/components/ProductList.tsx
@@ -0,0 +1,66 @@
+import { useMemo } from 'react';
+import { ColumnDef } from '@tanstack/react-table';
+import { generatePath } from 'react-router-dom';
+import { Loading, Table } from '@/components/Elements';
+import { useFilters } from '@/hooks/useFilters';
+import { Actions, generateDefaultActions } from '@/components/Elements/Table';
+import { useVariations } from '@/features/products/api/variations/getVariations';
+import { ProductVariationModel } from '@/features/products/types';
+import { StockRoutes } from '../routes/constants';
+
+function ProductList() {
+  const { query } = useFilters();
+  const variations = useVariations({ params: query });
+
+  const columns = useMemo<ColumnDef<ProductVariationModel>[]>(
+    () => [
+      {
+        header: 'Nome do produto',
+        accessorKey: 'variation',
+      },
+      {
+        header: 'Qtd.',
+        accessorKey: 'stock',
+        enableSorting: false,
+        cell: (info) => {
+          const stock = info.getValue() as { quantity: number } | null;
+          return stock ? stock.quantity : '-';
+        },
+      },
+      {
+        header: 'Ações',
+        accessorKey: 'id',
+        enableSorting: false,
+        cell: (info) => {
+          const id = info.getValue() as string;
+          return (
+            <Actions
+              id={id}
+              options={generateDefaultActions(
+                generatePath(StockRoutes.View, { id }),
+                generatePath(StockRoutes.Edit, { id })
+              )}
+            />
+          );
+        },
+      },
+    ],
+    []
+  );
+
+  if (variations.isFetching || variations.isLoading) {
+    return <Loading />;
+  } else if (!variations.data) {
+    return null;
+  }
+
+  return (
+    <Table
+      columns={columns}
+      data={variations.data.data || []}
+      pages={variations.data.meta.totalPages || 0}
+    />
+  );
+}
+
+export { ProductList };
