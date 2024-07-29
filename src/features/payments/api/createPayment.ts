d008b7488feca91aef665102d909dbc19bdd75ef
jlucasoliveira <lucasosilva68@gmail.com>
2024-07-29 12:03:15 -0300

    feat(payments): add create request

---
+++ src/features/payments/api/createPayment.ts
@@ -0,0 +1,40 @@
+import { useMutation } from '@tanstack/react-query';
+import { axios } from '@/lib/axios';
+import { MutationConfig, queryClient } from '@/lib/react-query';
+import { useNotificationStore } from '@/stores/notifications';
+import { PaymentModel } from '../types';
+
+export type Payload = {
+  value: number;
+  orderId?: string;
+  clientId: string;
+};
+
+async function createPayment(payload: Payload) {
+  const { data } = await axios.post<PaymentModel>(`/payments`, payload);
+  return data;
+}
+
+type UseCreatePayment = {
+  config?: MutationConfig<typeof createPayment>;
+};
+
+function useCreatePayment({ config }: UseCreatePayment = {}) {
+  const { addNotification } = useNotificationStore();
+
+  return useMutation({
+    ...config,
+    mutationKey: ['create-payment'],
+    mutationFn: (payload) => createPayment(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['fetch-payments']);
+      addNotification({
+        type: 'success',
+        title: 'Sucesso!',
+        message: 'Pagamento criado',
+      });
+    },
+  });
+}
+
+export { createPayment, useCreatePayment };
